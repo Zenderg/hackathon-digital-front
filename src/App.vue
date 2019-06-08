43d135e9c01362a4ada80b688cfbd9f81470e7f6
Zenderg <danil54658798@gmail.com>
2019-06-09 01:17:47 +0300

    add base structure event

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
     <div id="app">
-        <!--        <router-link to="/">Home</router-link> |-->
-        <!--        <router-link to="/profile">About</router-link>-->
+
         <router-view/>
     </div>
 </template>
--- src/views/Event.vue
+++ src/views/Event.vue
@@ -1,8 +1,41 @@
 <template>
     <div class="event">
-        <p class="category">
-            {{event.category.name}}
-        </p>
+        <div class="wrapper">
+            <p class="category">
+                {{event.category.name || null}}
+            </p>
+            <div class="info">
+                <div class="name-age">
+                    <p class="name">
+                        {{ event.name }}
+                    </p>
+                    <p class="age">
+                        <span>+{{ event.age_limit }}</span>
+                    </p>
+                </div>
+                <div class="desc">
+                    <p>{{ event.description }}</p>
+                </div>
+                <div class="short-info">
+                    <img class="icon" src="../assets/time.svg" alt="">
+                    <p class="text">{{ event.created_at }}</p>
+                </div>
+                <div class="short-info">
+                    <img class="icon" src="../assets/map_point.svg" alt="">
+                    <p class="text">{{ event.address }}</p>
+                </div>
+                <div class="short-info">
+                    <img class="icon" src="../assets/phone.svg" alt="">
+                    <p class="text">+8 800 555 35 35</p>
+                </div>
+                <button class="price">
+                    {{ event.price || "бесплатно" }}
+                </button>
+            </div>
+        </div>
+        <div class="map">
+            <img src="../assets/map_event.svg" alt="">
+        </div>
     </div>
 </template>
 
@@ -18,7 +51,7 @@
         },
         mounted() {
             axios.get("http://penka.studio/api/events/"+this.$route.params.id).then((r) => {
-                // console.log(r.data);
+                console.log(r.data);
                 this.event = r.data;
             })
         }
@@ -27,15 +60,79 @@
 
 <style scoped lang="scss">
 .event{
-    padding: 15px 30px 0 30px;
     background: linear-gradient(86.82deg, #40C6DD 0%, #A2E4EF 100%);
     box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.25);
-    .category{
-        background: rgba(255, 255, 255, 0.69);
-        padding: 5px 15px;
-        border-radius: 30px;
-        margin: 0;
-        display: inline;
+    .wrapper{
+        padding: 15px 30px 24px 30px;
+        .category{
+            background: rgba(255, 255, 255, 0.69);
+            padding: 5px 15px;
+            border-radius: 30px;
+            display: inline;
+        }
+        .info{
+            margin-top: 25px;
+            .name-age{
+                margin-bottom: 10px;
+                width: 100%;
+                display: flex;
+                .name{
+                    font-size: 24px;
+                    margin: 0;
+                    color: #FFFFFF;
+                }
+                .age{
+                    color: #FFFFFF;
+                    border: 2px solid #FFFFFF;
+                    height: 35px;
+                    width: 35px;
+                    text-align: center;
+                    border-radius: 50%;
+                    display: flex;
+                    font-size: 12px;
+                    margin-bottom: 0;
+                    margin-right: 0;
+                    margin-left: auto;
+                    span{
+                        margin: auto;
+                    }
+                }
+            }
+            .desc{
+                max-width: 250px;
+                margin-bottom: 20px;
+                p{
+                    color:#ffffff;
+                }
+            }
+            .short-info{
+                display: flex;
+                margin-bottom: 8px;
+                align-items: center;
+                .text{
+                    margin-bottom: 0;
+                    margin-left: 6px;
+                    color: #FFFFFF;
+                    font-size: 14px;
+                }
+            }
+            .price{
+                margin-top: 16px;
+                padding: 8px 25px;
+                background: #F9FF00;
+                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25);
+                border-radius: 12px;
+                font-size: 12px;
+                border: none;
+                cursor: pointer;
+                outline: none;
+            }
+        }
+    }
+    .map{
+        img{
+            width: 100%;
+        }
     }
 }
 </style>

f177bda4106d55a862c426c22fc29d3a6d72c2c2
Zenderg <danil54658798@gmail.com>
2019-06-09 14:51:19 +0300

    hotfix

--- src/App.vue
+++ src/App.vue
@@ -20,28 +20,31 @@
 </script>
 
 <style lang="scss" scoped>
-    .header {
-        width: 100%;
-        padding: 20px;
-        display: flex;
-        align-items: center;
-        border-bottom: 1px solid #eee;
-        box-shadow: 0px 5px 20px #eee;
+    #app{
+        padding: 0 0 30px;
+        .header {
+            width: 100%;
+            padding: 20px;
+            display: flex;
+            align-items: center;
+            border-bottom: 1px solid #eee;
+            box-shadow: 0px 5px 20px #eee;
 
-        .title {
-            margin: 0;
-            color: #000;
-            font-size: 20px;
-            font-weight: bold;
-        }
+            .title {
+                margin: 0;
+                color: #000;
+                font-size: 20px;
+                font-weight: bold;
+            }
 
-        .profile{
-            margin-left: auto;
-            img{
-                height: 34px;
-                width: 34px;
-                object-fit: cover;
-                border-radius: 50%;
+            .profile{
+                margin-left: auto;
+                img{
+                    height: 34px;
+                    width: 34px;
+                    object-fit: cover;
+                    border-radius: 50%;
+                }
             }
         }
     }
